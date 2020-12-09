ea9e1c76602e52cee78bdb240563e641c5dffa61
Zoey Farago <zf.zoodle@gmail.com>
2020-12-09 04:32:32 +0000

    Array loop ex.

--- loopyLighthouseProject.js
+++ loopyLighthouseProject.js
@@ -4,6 +4,18 @@ If the number is a multiple of 4, print the string "Lighthouse" instead of the n
 If the number is a multiple of both 3 and 4, print the string "LoopyLighthouse" instead of the number.
 By print, we are referring to console.log.*/
 
+/*range should be an array of 2 numbers representing the start and end values for the loop.
+multiples should be an array of 2 numbers representing the multiples you want to replace with words.
+words should be an array of 2 strings representing the words that will replace the multiples*/
+
 for(let n = 100; n <= 200; n++){
   n % 3 === 0? (n % 4 === 0? console.log("LoopyLighthouse") : console.log("Loopy")) : (n % 4 === 0? console.log("Lighthouse") : console.log(n));
 }
+
+function loopyLighthouse(range, multiples, words){
+  for(let i = range[0]; i <= range[1]; i++){
+    i % multiples[0] === 0? (i % multiples[1] === 0 ? console.log(words[0] + words [1]) : console.log(words[0])) : i % multiples[1] === 0? console.log(words[1]) : console.log(i);
+  }
+}
+
+console.log(loopyLighthouse([1,100],[2,5],["banana","pineapple"]));
---
+++ range.js
@@ -0,0 +1,23 @@
+function range(start,end,step){
+  let finalArray = [];
+  if( start === undefined|| end === undefined|| step === undefined || start > end || step <= 0 ){
+    finalArray = [];
+  } else {
+    for(let i = start; i <= end; i += step){
+      finalArray.push(i);
+    }
+  }
+  return finalArray;
+}
+
+
+console.log(range(0, 10, 0));
+console.log(range(10, 30, 5));
+console.log(range(-5, 2, 3));
+
+/*The function should return an empty array [] if given incorrect parameters, such as:
+
+start, end, or step being undefined
+start being greater than end
+step being 0, or negative*/
+
