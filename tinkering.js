33c95ce72181b450498e5817b02ea96c128e9b80
Zoey Farago <zf.zoodle@gmail.com>
2020-12-14 15:30:28 +0000

    using a function to sort an array numerically - descending

--- tinkering.js
+++ tinkering.js
@@ -1,6 +1,15 @@
+/*
 console.log(true || false);
 console.log(false && false);
 console.log(!true);
 console.log(13> -7 || (false == 0));
 console.log((10==="10")&& (1<=2));
 console.log((3 != 6 % 3) && !(24>45)&&(!false));
+*/
+
+var numArray = [140000, 104, 99];
+numArray.sort(function(a, b) {
+  return b - a;
+});
+
+console.log(numArray);
